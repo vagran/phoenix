286ae5ac5ff92d4b49674d4820ba398f12a8259c
vagran <vagran.ast@gmail.com>
2011-10-20 15:38:42 +0300

    Read/write spin locks implementation added.

--- kernel/sys/arch/x86_64/lock.h
+++ kernel/sys/arch/x86_64/lock.h
@@ -32,12 +32,12 @@
  */
 class SpinLock {
 private:
-    u32 _flag;
+    volatile u32 _flag;
 public:
     /**
      * @param isLocked Initial state of the lock.
      */
-    inline SpinLock(bool isLocked = 0) { _flag = isLocked ? 1 : 0; }
+    inline SpinLock() { _flag = 0; }
 
     inline ~SpinLock() { ASSERT(!_flag); }
 
@@ -90,6 +90,77 @@ public:
      * @return Current state of the lock - true if locked, false if not locked.
      */
     inline operator bool() { return _flag ? true : false; }
+};
+
+/**
+ * Read/write spin lock. It allows several simultaneous reads but doesn't allow
+ * write simultaneously with another read or write.
+ */
+class RWSpinLock {
+private:
+    /* Higher bit indicates write lock. Lower bits store number of read locks
+     * acquired or pending.
+     */
+    enum {
+        WRITE_LOCK =    0x80000000,
+        READ_LOCK =     0x7fffffff
+    };
+
+    volatile u32 _state;
+public:
+    inline RWSpinLock() { _state = 0; }
+    inline ~RWSpinLock() { ASSERT(!_state); }
+
+    /** Acquire read lock. Several simultaneous read locks can be acquired. */
+    inline void ReadLock() {
+        ASM(
+            "lock incl %[state]\n" /* Create pending read. */
+            :
+            : [state]"m"(_state)
+            : "cc");
+        /* Wait until write lock released. */
+        while (_state & WRITE_LOCK) {
+            cpu::pause();
+        }
+    }
+
+    /** Release read lock. */
+    inline void ReadUnlock() {
+        ASSERT(_state & READ_LOCK);
+        ASM(
+            "lock decl %[state]\n" /* Decrement pending read. */
+            :
+            : [state]"m"(_state)
+            : "cc");
+    }
+
+    /** Acquire write lock. It can be acquired only exclusively. It will not be
+     * acquired after previous write lock released and there are pending read
+     * locks.
+     */
+    inline void WriteLock() {
+        u32 cur_state;
+        ASM(
+            /* Wait until there are no pending reads and write lock is not acquired. */
+            "1: xorl %%eax, %%eax\n"
+            "lock cmpxchgl %[write_lock], %[state]\n"
+            "jz 2f\n"
+            "pause\n"
+            "jmp 1b\n"
+            "2:\n"
+            : "=a"(cur_state)
+            : [state]"m"(_state), [write_lock]"r"(WRITE_LOCK)
+            : "cc"
+            );
+    }
+
+    inline void WriteUnlock() {
+        ASM(
+            "lock andl %[write_lock], %[state]\n"
+            :
+            : [state]"m"(_state), [write_lock]"r"(~WRITE_LOCK)
+            : "cc");
+    }
 };
 
 #endif /* LOCK_H_ */
